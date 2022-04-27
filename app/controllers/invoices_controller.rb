d611e434c6bcbc3807c4f6e477328b02702a2d4a
renaud dor <renaud.dor@gmail.com>
2022-04-27 15:42:41 +0200

    Create invoice CRUD

---
+++ app/controllers/invoices_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal:true
+
+class InvoicesController < ApplicationController
+  before_action :set_invoice, only: %i[show edit update destroy]
+
+  def index
+    @invoices = Invoice.all
+  end
+
+  def show; end
+
+  def new
+    @invoice = Invoice.new
+  end
+
+  def create
+    @invoice = Invoice.new(invoice_params)
+
+    if @invoice.save
+      redirect_to invoices_path, notice: 'Invoice was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @invoice.update(invoice_params)
+      redirect_to invoices_path, notice: 'Invoice was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @invoice.destroy
+    redirect_to invoices_path, notice: 'Invoice was successfully destroyed.'
+  end
+
+  private
+
+  def set_invoice
+    @invoice = Invoice.find(params[:id])
+  end
+
+  def invoice_params
+    params.require(:invoice).permit(:name)
+  end
+end
--- test/system/invoices_test.rb
+++ test/system/invoices_test.rb
@@ -3,25 +3,58 @@
 require 'application_system_test_case'
 
 class InvoicesTest < ApplicationSystemTestCase
+  setup do
+    @invoice = invoices(:first) # Reference to the first fixture invoicee
+  end
+
   test 'Creating a new invoice' do
     # When we visit the Invoice#index page
     # we expect to see a title with the text 'Invoice'
-    visit invoice_path
-    assert_selector 'h1', text: 'Invoice'
+    visit invoices_path
+    assert_selector 'h1', text: 'Invoices'
 
-    # When we click on the link with the text 'New quote'
-    # we expect to land on a page with the title 'New quote'
-    click_on 'New quote'
-    assert_selector 'h1', text: 'New quote'
+    # When we click on the link with the text 'New invoice'
+    # we expect to land on a page with the title 'New invoice'
+    click_on 'New invoice'
+    assert_selector 'h1', text: 'New invoice'
 
-    # When we fill in the name input with 'Capybara quote'
-    # and we click on 'Create Quote'
-    fill_in 'Name', with: 'Capybara quote'
-    click_on 'Create quote'
+    # When we fill in the name input with 'Capybara invoice'
+    # and we click on 'Create Invoice'
+    fill_in 'Name', with: 'Capybara invoice'
+    click_on 'Create invoice'
 
     # We expect to be back on the page with the title 'Invoice'
-    # and to see our 'Capybara quote' added to the list
+    # and to see our 'Capybara invoice' added to the list
     assert_selector 'h1', text: 'Invoice'
-    assert_text 'Capybara quote'
+    assert_text 'Capybara invoice'
+  end
+
+  test 'Showing a invoice' do
+    visit invoices_path
+    click_link @invoice.name
+
+    assert_selector 'h1', text: @invoice.name
+  end
+
+  test 'Updating a invoice' do
+    visit invoices_path
+    assert_selector 'h1', text: 'Invoices'
+
+    click_on 'Edit', match: :first
+    assert_selector 'h1', text: 'Edit invoice'
+
+    fill_in 'Name', with: 'Updated invoice'
+    click_on 'Update invoice'
+
+    assert_selector 'h1', text: 'Invoices'
+    assert_text 'Updated invoice'
+  end
+
+  test 'Destroying a invoice' do
+    visit invoices_path
+    assert_text @invoice.name
+
+    click_on 'Delete', match: :first
+    assert_no_text @invoice.name
   end
 end
