0d41b0ae6b2cd96d0c38999acfe5e3110393eac2
renaud dor <renaud.dor@gmail.com>
2022-04-28 15:06:43 +0200

    Real-time updates with turbo streams

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,13 @@ class Invoice < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  after_create_commit -> { broadcast_prepend_later_to 'invoices', partial: 'invoices/invoice', locals: { invoice: self }, target: 'invoices' }
+  # can be shortened to after_create_commit -> { broadcast_prepend_to 'invoices' }
+
+  after_update_commit -> { broadcast_replace_later_to 'invoices' }
+  after_destroy_commit -> { broadcast_remove_to 'invoices' }
+
+  # Those three previous callbacks are equivalent to the following single line
+  # broadcasts_to ->(invoice) { "invoice" }, inserts_by: :prepend
 end
